7de50c500d508fa399530e2bfd25aeeff52ddf14
Lucas Coutinho <lucascgs1@hotmail.com>
2023-03-02 16:02:21 -0300

    feat: header da aplicacao e ajuste na sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Ailos';
+
+  hidenNotificationCount: boolean = true;
+  notification: any[] = [{}, {}];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.hidenNotificationCount = this.notification.length > 0 ? false : true;
+  }
 }
